a50ec19937fd78d854b79deddb0b12d25ac98421
Chu Tuấn Long <88946336+Longct2022@users.noreply.github.com>
2022-05-01 16:08:54 +0700

    Add files via upload

---
+++ Entities/SinhVien.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTLQLSV.Entities
+{
+    public class SinhVien
+    {
+        public string MaSV { get; set; }
+        public string TenSV { get; set; }
+        public string MaLop { get; set; }
+        public string QueQuan { get; set; }
+        public String NgaySinh { get; set; }
+    }
+}
---
+++ Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTLQLSV.Entities
+{
+    public class User
+    {
+        public string UserID { get; set; }
+        public string PassWord { get; set; }
+        public int Role { get; set; }
+    }
+}
