0d441686cfe45c0473f9d78c83a0d6bcf55eff53
Chu Tuấn Long <88946336+Longct2022@users.noreply.github.com>
2022-05-01 16:01:59 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BTLQLSV.GUI;
+using System.Configuration;
+using BTLQLSV.Entities;
+
+namespace BTLQLSV
+{
+    public static class Program
+    {
+        public static string stcon;
+        public static User user;
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            stcon = ConfigurationManager.ConnectionStrings["strcon"].ConnectionString;
+            Application.Run(new FrmDangNhap());
+        }
+    }
+}
