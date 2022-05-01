84289b13b5d64c79688af58fd44bdea3d34a5141
Chu Tuấn Long <88946336+Longct2022@users.noreply.github.com>
2022-05-01 16:03:35 +0700

    Add files via upload

---
+++ BUS/DangNhapBUS.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BTLQLSV.DAO;
+using BTLQLSV.Entities;
+using System.Data;
+
+namespace BTLQLSV.BUS
+{
+
+    public class DangNhapBUS
+    {
+        DataHelper dh;
+        public DangNhapBUS(string sqlcon)
+        {
+            dh = new DataHelper(sqlcon);
+        }
+        public User GetUser(string un, string pw)
+        {
+            DataTable dt = dh.FillDataTable("select * from Users WHERE (UserID ='" + un + "')" + "and (Password = '" + pw + "')");
+            if (dt.Rows.Count > 0)
+            {
+                User user = new User();
+                user.UserID = dt.Rows[0]["UserId"].ToString();
+                user.PassWord = dt.Rows[0]["Password"].ToString();
+                user.Role = int.Parse(dt.Rows[0]["Role"].ToString());
+                return user;
+            }
+            else return null;
+        }
+
+    }
+}
---
+++ BUS/QLSVBUS.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BTLQLSV.DAO;
+using BTLQLSV.Entities;
+using System.Windows;
+
+namespace BTLQLSV.BUS
+{
+    public class QLSVBUS
+    {
+        SinhVienDAO svd;
+        LopDAO lopd;
+        public QLSVBUS(string sqlcon)
+        {
+            svd = new SinhVienDAO(sqlcon);
+            lopd = new LopDAO(sqlcon);
+        }
+        public List<Lop> LayLop()
+        {
+            return lopd.LayDanhSachLop();
+        }
+        public List<SinhVien> LaySinhVienAll()
+        {
+            return svd.LaySinhVienAll();
+        }
+        public List<SinhVien> LaySinhVien(string malop)
+        {
+            return svd.LaySinhVien(malop);
+        }
+        public void NhapSinhVien(SinhVien sv)
+        {
+            lopd.SuaSiSo(1, sv.MaLop);
+            svd.ThemSinhVien(sv);
+        }
+        public void SuaSinhvien(SinhVien sv, string malopcu)
+        {
+            svd.SuaSinhVien(sv);
+            if (sv.MaLop != malopcu)
+            {
+                lopd.SuaSiSo(1, sv.MaLop);
+                lopd.SuaSiSo(-1, malopcu);
+            }
+        }
+        public void XoaSinhVien(string maSV)
+        {
+            svd.XoaSinhVien(maSV);
+        }
+        public void XoaSinhVienH(string maSV)
+        {
+            svd.XoaSinhVienH(maSV);
+        }
+    }
+    
+}
